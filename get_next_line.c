16ac77a12fb6ae30e053f4a10313dec808b9cd51
Miguel <miguel@MacBook-Pro-de-Miguel.local>
2025-05-05 17:37:02 +0100

    20250505

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,17 @@
 /*   By: josemigu <josemigu@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/05 15:57:00 by josemigu          #+#    #+#             */
-/*   Updated: 2025/05/05 15:57:01 by josemigu         ###   ########.fr       */
+/*   Updated: 2025/05/05 16:11:44 by josemigu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+char	*get_next_line(int fd)
+{
+	
+	static char BUFFER[BUFFER_SIZE];
+	
+	if (fd == -1)
+		return (NULL);
+}
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,20 @@
 /*   By: josemigu <josemigu@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/05 15:57:20 by josemigu          #+#    #+#             */
-/*   Updated: 2025/05/05 15:57:21 by josemigu         ###   ########.fr       */
+/*   Updated: 2025/05/05 16:14:37 by josemigu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+char	*get_next_line(int fd);
+
+#endif
